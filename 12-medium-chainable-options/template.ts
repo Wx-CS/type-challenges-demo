57a0c7f9004645adeb3aa3d83823245eccc5c373
Wx-CS <66178162+Wx-CS@users.noreply.github.com>
2022-05-31 23:02:37 +0800

    feat: chainable-options

---
+++ 12-medium-chainable-options/template.ts
@@ -0,0 +1,5 @@
+type Chainable<T extends object = {}> = {
+  option<K extends string, V extends unknown>(key: K extends keyof T ? never: K, value: V): 
+  Chainable<T & {[P in K]: V}>,
+  get(): T
+}
