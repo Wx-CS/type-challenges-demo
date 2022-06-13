f9462282689cea49a85f824fd22f90a0613f3086
Wx-CS <66178162+Wx-CS@users.noreply.github.com>
2022-06-13 22:54:45 +0800

    feat: replacekeys

---
+++ 1130-medium-replacekeys/test-cases.ts
@@ -0,0 +1,56 @@
+import type { Equal, Expect } from '@type-challenges/utils'
+
+type NodeA = {
+  type: 'A'
+  name: string
+  flag: number
+}
+type NodeB = {
+  type: 'B'
+  id: number
+  flag: number
+}
+type NodeC = {
+  type: 'C'
+  name: string
+  flag: number
+}
+
+type ReplacedNodeA = {
+  type: 'A'
+  name: number
+  flag: string
+}
+type ReplacedNodeB = {
+  type: 'B'
+  id: number
+  flag: string
+}
+type ReplacedNodeC = {
+  type: 'C'
+  name: number
+  flag: string
+}
+
+type NoNameNodeA = {
+  type: 'A'
+  flag: number
+  name: never
+}
+type NoNameNodeC = {
+  type: 'C'
+  flag: number
+  name: never
+}
+
+type Nodes = NodeA | NodeB | NodeC
+type ReplacedNodes = ReplacedNodeA | ReplacedNodeB | ReplacedNodeC
+type NodesNoName = NoNameNodeA | NoNameNodeC | NodeB
+
+type cases = [
+  Expect<Equal<ReplaceKeys<Nodes, 'name' | 'flag', { name: number; flag: string }>, ReplacedNodes>>,
+  // {type: 'A', name: number, flag: string} | {type: 'B', id: number, flag: string} | {type: 'C', name: number, flag: string} // would replace name from string to number, replace flag from number to string.
+  Expect<Equal<ReplaceKeys<Nodes, 'name', { aa: number }>, NodesNoName>>,
+  // {type: 'A', name: never, flag: number} | NodeB | {type: 'C', name: never, flag: number} // would replace name to never
+
+]
